752909dd52bd9b1f6788c5e5e238fd9a63362229
KH <kasperkh.kh@gmail.com>
2017-12-06 10:00:15 +0100

    Fixed background-image SVGs not showing due to Firefox bug

--- docs/todo.js
+++ docs/todo.js
@@ -1,22 +1,25 @@
 websiteCode();
 globalCode();
 
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-// Safari 3.0+ "[object HTMLElementConstructor]"
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-// Chrome 1+
-var isChrome = !!window.chrome && !!window.chrome.webstore;
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 function websiteCode() {
+    (function browserTests() {
+        // Opera 8.0+
+        window.isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+        // Firefox 1.0+
+        window.isFirefox = typeof InstallTrigger !== 'undefined';
+        // Safari 3.0+ "[object HTMLElementConstructor]"
+        window.isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+        // Internet Explorer 6-11
+        window.isIE = /*@cc_on!@*/false || !!document.documentMode;
+        // Edge 20+
+        window.isEdge = !isIE && !!window.StyleMedia;
+        // Chrome 1+
+        window.isChrome = !!window.chrome && !!window.chrome.webstore;
+        // Blink engine detection
+        window.isBlink = (isChrome || isOpera) && !!window.CSS;
+    })();
+
     if (isChrome) {
         var chromeExtensionDiv = document.querySelector(".chrome-extension");
         chromeExtensionDiv.classList.add("visible");
