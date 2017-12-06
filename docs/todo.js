6e988ba8a6855a05efd749fac607f8cb70341787
KH <kasperkh.kh@gmail.com>
2017-12-06 09:43:08 +0100

    Fix where Firefox throws referenceError

--- docs/todo.js
+++ docs/todo.js
@@ -1,7 +1,23 @@
 websiteCode();
 globalCode();
+
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+// Safari 3.0+ "[object HTMLElementConstructor]"
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
 function websiteCode() {
-    if (chrome && chrome.webstore && chrome.webstore.install) {
+    if (isChrome) {
         var chromeExtensionDiv = document.querySelector(".chrome-extension");
         chromeExtensionDiv.classList.add("visible");
         var svg = chromeExtensionDiv.querySelector(" svg");
