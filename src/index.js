a42f29d7730aaf506aee317f4501115c3b85127c
Kasper <kasperkh.kh@gmail.com>
2018-11-07 22:44:07 +0100

    Using Quill.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,68 @@
-const textarea = document.querySelector('textarea')
+const noteDiv = document.querySelector('#note')
 
-function resizeTextarea () {
-  textarea.style.height = 'auto'
-  const paddingTop = parseInt(getComputedStyle(textarea).paddingTop)
-  const paddingBottom = parseInt(getComputedStyle(textarea).paddingTop)
-  textarea.style.height = (textarea.scrollHeight - paddingTop - paddingBottom) + 'px'
+const content = localStorage.getItem('note')
+if (content) {
+  noteDiv.innerHTML = content
 }
 
-textarea.style.overflowY = 'hidden'
-textarea.value = JSON.parse(localStorage.getItem('content')).note
-resizeTextarea()
-textarea.addEventListener('input', function () {
-  resizeTextarea()
-}, false)
-
-function save () {
-  const content = {
-    time: new Date().toString(),
-    note: textarea.value
-  }
-  localStorage.setItem('content', JSON.stringify(content))
-}
-
-document.addEventListener('input', function (e) {
-  const element = e.target
-  if (element.nodeName === 'TEXTAREA') {
-    save()
-  }
+const quill = new Quill(noteDiv, {
+  modules: {
+    toolbar: { container: '#toolbar-container' },
+    history: {
+      maxStack: 1000
+    }
+  },
+  theme: 'snow',
+  placeholder: 'Maybe I\'ll have a todo list here?'
 })
 
-// 'settingsDialog'
+function save () {
+  localStorage.setItem('note', quill.container.innerHTML)
+  localStorage.setItem('noteTime', new Date().toString())
+}
+
+quill.on('text-change', save)
+
+// const textarea = document.querySelector('textarea')
+
+// function resizeTextarea () {
+//   textarea.style.height = 'auto'
+//   const paddingTop = parseInt(getComputedStyle(textarea).paddingTop)
+//   const paddingBottom = parseInt(getComputedStyle(textarea).paddingTop)
+//   textarea.style.height = (textarea.scrollHeight - paddingTop - paddingBottom) + 'px'
+// }
+
+// textarea.style.overflowY = 'hidden'
+// const content = localStorage.getItem('content')
+// if (content) {
+//   quill.setContents(JSON.parse(content.note))
+// }
+// resizeTextarea()
+// textarea.addEventListener('input', function () {
+//   resizeTextarea()
+// }, false)
+
+// function save (delta) {
+//   if (!delta) delta = quill.getContents()
+//   const content = {
+//     time: new Date().toString(),
+//     note: delta
+//   }
+//   localStorage.setItem('content', JSON.stringify(content))
+// }
+
+// document.addEventListener('input', function (e) {
+//   const element = e.target
+//   if (element.nodeName === 'TEXTAREA') {
+//     save()
+//   }
+// })
+// quill.on('text-change', function (delta, oldDelta, source) {
+//   console.log(000)
+//   save(delta)
+// })
+
+// settingsDialog
 const svg = document.querySelector('.settings svg')
 const dialog = document.querySelector('.settings-dialog')
 svg.addEventListener('click', () => {
