60513fa7c253c5cd445661c0909069f51af9bb66
Kasper <kasperkh.kh@gmail.com>
2018-11-17 23:41:55 +0100

    Not building Quill anymore

--- src/lib/quill.js
+++ src/lib/quill.js
@@ -1,105 +1,109 @@
-import Quill from 'quill/core'
+// import Quill from 'quill/core'
 
-import { AlignClass, AlignStyle } from 'quill/formats/align'
-import { DirectionAttribute, DirectionClass, DirectionStyle } from 'quill/formats/direction'
-import Indent from 'quill/formats/indent'
+// import { AlignClass, AlignStyle } from 'quill/formats/align'
+// import { DirectionAttribute, DirectionClass, DirectionStyle } from 'quill/formats/direction'
+// import Indent from 'quill/formats/indent'
 
-import Blockquote from 'quill/formats/blockquote'
-import Header from 'quill/formats/header'
-import List, { ListItem } from 'quill/formats/list'
+// import Blockquote from 'quill/formats/blockquote'
+// import Header from 'quill/formats/header'
+// import List, { ListItem } from 'quill/formats/list'
 
-import { BackgroundClass, BackgroundStyle } from 'quill/formats/background'
-import { ColorClass, ColorStyle } from 'quill/formats/color'
-import { FontClass, FontStyle } from 'quill/formats/font'
-import { SizeClass, SizeStyle } from 'quill/formats/size'
+// import { BackgroundClass, BackgroundStyle } from 'quill/formats/background'
+// import { ColorClass, ColorStyle } from 'quill/formats/color'
+// import { FontClass, FontStyle } from 'quill/formats/font'
+// import { SizeClass, SizeStyle } from 'quill/formats/size'
 
-import Bold from 'quill/formats/bold'
-import Italic from 'quill/formats/italic'
-import Link from 'quill/formats/link'
-import Script from 'quill/formats/script'
-import Strike from 'quill/formats/strike'
-import Underline from 'quill/formats/underline'
+// import Bold from 'quill/formats/bold'
+// import Italic from 'quill/formats/italic'
+// import Link from 'quill/formats/link'
+// import Script from 'quill/formats/script'
+// import Strike from 'quill/formats/strike'
+// import Underline from 'quill/formats/underline'
 
-import Image from 'quill/formats/image'
-import Video from 'quill/formats/video'
+// import Image from 'quill/formats/image'
+// import Video from 'quill/formats/video'
 
-import CodeBlock, { Code as InlineCode } from 'quill/formats/code'
+// import CodeBlock, { Code as InlineCode } from 'quill/formats/code'
 
-import Syntax from 'quill/modules/syntax'
-import Toolbar from 'quill/modules/toolbar'
+// import Syntax from 'quill/modules/syntax'
+// import Toolbar from 'quill/modules/toolbar'
 
-import Icons from 'quill/ui/icons'
-import Picker from 'quill/ui/picker'
-import ColorPicker from 'quill/ui/color-picker'
-import IconPicker from 'quill/ui/icon-picker'
-import Tooltip from 'quill/ui/tooltip'
+// import Icons from 'quill/ui/icons'
+// import Picker from 'quill/ui/picker'
+// import ColorPicker from 'quill/ui/color-picker'
+// import IconPicker from 'quill/ui/icon-picker'
+// import Tooltip from 'quill/ui/tooltip'
 
-import SnowTheme from 'quill/themes/snow'
+// import SnowTheme from 'quill/themes/snow'
 
+// import MagicUrl from 'quill-magic-url'
+
+// Quill.register(
+//   {
+//     'attributors/attribute/direction': DirectionAttribute,
+
+//     'attributors/class/align': AlignClass,
+//     'attributors/class/background': BackgroundClass,
+//     'attributors/class/color': ColorClass,
+//     'attributors/class/direction': DirectionClass,
+//     'attributors/class/font': FontClass,
+//     'attributors/class/size': SizeClass,
+
+//     'attributors/style/align': AlignStyle,
+//     'attributors/style/background': BackgroundStyle,
+//     'attributors/style/color': ColorStyle,
+//     'attributors/style/direction': DirectionStyle,
+//     'attributors/style/font': FontStyle,
+//     'attributors/style/size': SizeStyle,
+//   },
+//   true,
+// )
+
+// Quill.register(
+//   {
+//     'formats/align': AlignClass,
+//     // 'formats/direction': DirectionClass,
+//     // 'formats/indent': Indent,
+
+//     // 'formats/background': BackgroundStyle,
+//     // 'formats/color': ColorStyle,
+//     // 'formats/font': FontClass,
+//     // 'formats/size': SizeClass,
+
+//     // 'formats/blockquote': Blockquote,
+//     // 'formats/code-block': CodeBlock,
+//     'formats/header': Header,
+//     'formats/list': List,
+//     'formats/list-item': ListItem,
+//     'formats/bold': Bold,
+//     // 'formats/code': InlineCode,
+//     'formats/italic': Italic,
+//     'formats/link': Link,
+//     // 'formats/script': Script,
+//     'formats/strike': Strike,
+//     'formats/underline': Underline,
+
+//     // 'formats/image': Image,
+//     // 'formats/video': Video,
+
+//     // 'modules/syntax': Syntax,
+//     'modules/toolbar': Toolbar,
+
+//     'themes/snow': SnowTheme,
+
+//     // 'ui/icons': Icons,
+//     // 'ui/picker': Picker,
+//     // 'ui/icon-picker': IconPicker,
+//     // 'ui/color-picker': ColorPicker,
+//     // 'ui/tooltip': Tooltip,
+
+//     'modules/magicUrl': MagicUrl,
+//   },
+//   true,
+// );
+
+import Quill from 'quill/quill'
 import MagicUrl from 'quill-magic-url'
-
-Quill.register(
-  {
-    'attributors/attribute/direction': DirectionAttribute,
-
-    'attributors/class/align': AlignClass,
-    'attributors/class/background': BackgroundClass,
-    'attributors/class/color': ColorClass,
-    'attributors/class/direction': DirectionClass,
-    'attributors/class/font': FontClass,
-    'attributors/class/size': SizeClass,
-
-    'attributors/style/align': AlignStyle,
-    'attributors/style/background': BackgroundStyle,
-    'attributors/style/color': ColorStyle,
-    'attributors/style/direction': DirectionStyle,
-    'attributors/style/font': FontStyle,
-    'attributors/style/size': SizeStyle,
-  },
-  true,
-)
-
-Quill.register(
-  {
-    'formats/align': AlignClass,
-    // 'formats/direction': DirectionClass,
-    // 'formats/indent': Indent,
-
-    // 'formats/background': BackgroundStyle,
-    // 'formats/color': ColorStyle,
-    // 'formats/font': FontClass,
-    // 'formats/size': SizeClass,
-
-    // 'formats/blockquote': Blockquote,
-    // 'formats/code-block': CodeBlock,
-    'formats/header': Header,
-    'formats/list': List,
-    'formats/list-item': ListItem,
-    'formats/bold': Bold,
-    // 'formats/code': InlineCode,
-    'formats/italic': Italic,
-    'formats/link': Link,
-    // 'formats/script': Script,
-    'formats/strike': Strike,
-    'formats/underline': Underline,
-
-    // 'formats/image': Image,
-    // 'formats/video': Video,
-
-    // 'modules/syntax': Syntax,
-    'modules/toolbar': Toolbar,
-
-    'themes/snow': SnowTheme,
-
-    // 'ui/icons': Icons,
-    // 'ui/picker': Picker,
-    // 'ui/icon-picker': IconPicker,
-    // 'ui/color-picker': ColorPicker,
-    // 'ui/tooltip': Tooltip,
-
-    'modules/magicUrl': MagicUrl,
-  },
-  true,
-);
+Quill.register('modules/magicUrl', MagicUrl)
 
 export default Quill
